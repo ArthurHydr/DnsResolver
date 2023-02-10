4b4ed8c5a43fb9f4017389465adc2841181d4cef
ArthurHydr <61481946+ArthurHydr@users.noreply.github.com>
2023-02-10 05:21:46 +0000

    Added initial dnsResolver file

---
+++ dnsResolver.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+
+int main(int argc, char *argv[]){
+    
+    if(argc < 2) {
+        printf("Use: dnsResolver <domain> \n");
+
+    } else {
+
+        struct hostent *host = gethostbyname(argv[1]);
+        char *getIP = inet_ntoa(*((struct in_addr *)host -> h_addr));
+
+        printf("IP: %s\n", getIP);
+    }
+    return 0;
+}
