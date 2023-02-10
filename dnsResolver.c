020baabf943a41d80f71372b8e84dfed3e5dab6d
ArthurHydr <61481946+ArthurHydr@users.noreply.github.com>
2023-02-10 05:27:31 +0000

    Added if filters

--- dnsResolver.c
+++ dnsResolver.c
@@ -6,13 +6,17 @@ int main(int argc, char *argv[]){
     
     if(argc < 1) {
         printf("Use: dnsResolver <domain> \n");
+        return 0;
 
     } else {
 
         struct hostent *host = gethostbyname(argv[1]);
         char *getIP = inet_ntoa(*((struct in_addr *)host -> h_addr));
-
-        printf("IP: %s\n", getIP);
+        
+        if (host == NULL) {
+            printf("Error\n");
+        } else {
+            printf("IP: %s\n", getIP);
+        }
     }
-    return 0;
 }
